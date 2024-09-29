e0189952e158b7c2747e3bf633692c408a8c4bd1
luizaa-m <luiza.martins0310@proton.me>
2024-09-29 17:02:40 -0300

    initial commit

---
+++ src/main/java/com/luizamartins/life_manager/LifeManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.luizamartins.life_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizamartins/life_manager/LifeManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizamartins.life_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
