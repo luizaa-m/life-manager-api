9149114c8ddaad36511c8243b107588f0e9ea41d
luizaa-m <luiza.martins0310@proton.me>
2024-10-13 19:32:54 -0300

    feature: create crud functions categoriagasto

--- src/main/java/com/luizamartins/life_manager/categoriagasto/CategoriaGastoService.java
+++ src/main/java/com/luizamartins/life_manager/categoriagasto/CategoriaGastoService.java
@@ -1,20 +1,46 @@
 package com.luizamartins.life_manager.categoriagasto;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoriaGastoService {
     private final CategoriaGastoRepository categoriaGastoRepository;
 
     @Autowired
-    private CategoriaGastoService(CategoriaGastoRepository categoriaGastoRepository){
+    protected CategoriaGastoService(CategoriaGastoRepository categoriaGastoRepository){
         this.categoriaGastoRepository=categoriaGastoRepository;
     }
 
     public List<CategoriaGasto> findAll(){
         return categoriaGastoRepository.findAll();
+    }
+
+    public Page<CategoriaGasto> findAllByPageable(Pageable pageable){
+        return categoriaGastoRepository.findAll(pageable);
+    }
+
+    @Transactional
+    public CategoriaGasto create(CategoriaGasto categoriaGasto){
+        return categoriaGastoRepository.save(categoriaGasto);
+    }
+
+    @Transactional
+    public void delete(long id){
+        categoriaGastoRepository.deleteById(id);
+    }
+
+    @Transactional
+    public CategoriaGasto update(CategoriaGasto categoriaGasto){
+        if(!categoriaGastoRepository.existsById(categoriaGasto.getId())){
+            throw new RuntimeException();
+        }
+        return categoriaGastoRepository.save(categoriaGasto);
     }
 }
